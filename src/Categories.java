60be96a039776d8b435d4d6d19ac1ead237216e1
LEB-Atheel <ado@lebsystem.se>
2024-11-19 19:06:33 +0100

    Skapat enum med 10 olika kategorier

--- src/Categories.java
+++ src/Categories.java
@@ -1,2 +1,23 @@
 public enum Categories {
+
+
+        JORDEN_RUNT ("Jorden runt"),
+        KONST_OCH_KULTUR ("Konst & kultur"),
+        I_LABBET ("I labbet"),
+        HISTORIA ("Historia"),
+        MAT_OCH_DRYCK ("Mat & dryck"),
+        KROPP_OCH_KNOPP("Kropp & knopp"),
+        TEKNIKENS_UNDER ("Teknikens under"),
+        TV_SERIER ("TV_serier"),
+        SPORT_OCH_FRITID ("Sport & fritid"),
+        DJUR_OCH_NATUR ("Djur & natur");
+
+        public final String category;
+
+        Categories (String c){
+            category = c;
+        }
 }
+
+
+
