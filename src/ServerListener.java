cf3007526502f367fbda9c3e2aeec4af22afa9fb
PauliLinde <jus.paulina@gmail.com>
2024-11-21 09:59:51 +0100

    Small changes in serverListener

--- src/ServerListener.java
+++ src/ServerListener.java
@@ -6,14 +6,15 @@ public class ServerListener {
 
     private MultiUser multiUser = new MultiUser();
 
-    public ServerListener(){
-
-        try(ServerSocket serverSocket = new ServerSocket(8080);) {
+    public ServerListener() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        try{
             while (true) {
-                Socket socket = serverSocket.accept();
-
-                Server server = new Server(socket, multiUser);
-                server.start();
+                //Socket socket = serverSocket.accept();
+                Server player1 = new Server(serverSocket.accept(), multiUser);
+                Server player2 = new Server(serverSocket.accept(), multiUser);
+                player1.start();
+                player2.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
