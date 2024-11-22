aaa7dd38cd3cd6dac1ec6d1eefd4408c826329f1
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2024-11-22 18:51:44 +0100

    Fixat GameProcess så att den samarbetar med properties-filen

--- src/Protocol.java
+++ src/Protocol.java
@@ -8,7 +8,7 @@ public class Protocol {
 
     Database database = new Database();
 
-    Categories [] genres = {Categories.JORDEN_RUNT, Categories.KONST_OCH_KULTUR,
+    Categories[] genres = {Categories.JORDEN_RUNT, Categories.KONST_OCH_KULTUR,
             Categories.I_LABBET, Categories.HISTORIA, Categories.MAT_OCH_DRYCK,
             Categories.KROPP_OCH_KNOPP, Categories.TEKNIKENS_UNDER,
             Categories.TV_SERIER, Categories.SPORT_OCH_FRITID, Categories.DJUR_OCH_NATUR};
@@ -16,17 +16,18 @@ public class Protocol {
     Properties prop = new Properties();
     int questionsInRound;
     int roundsInGame;
+    int questionsAsked = 0;
 
     List<QuestionsAndAnswers> currentGenre;
     private static final int GENRE = 0;
     private static final int QUESTION1 = 1;
     private static final int QUESTION2 = 2;
+    private static final int QUESTION3 = 3;
     private int state = GENRE;
 
-    //Setup som behöver anropas från en sammanbindande logik-metod, och sen användas på något sätt
-    //Skapar två int som läser in hur många frågor det ska vara i varje runda och hur många rundor totalt
+    //Initierar spelet
     public void gameSetup() {
-        try(FileInputStream fs = new FileInputStream("game_settings.properties")) {
+        try (FileInputStream fs = new FileInputStream("game_settings.properties")) {
             prop.load(fs);
             questionsInRound = Integer.parseInt(prop.getProperty("questionsInRound")); //3
             roundsInGame = Integer.parseInt(prop.getProperty("roundsInGame")); //6
@@ -35,44 +36,61 @@ public class Protocol {
         }
     }
 
-    //Behöver också anropas från sammanbindande logik-metod
-    public QuestionsAndAnswers gameProcess(String theInput){
-        QuestionsAndAnswers theOutput = null;
-        Categories chosenGenre;
-
-        /*Här tror jag att en justering kommer behövas med states
+           /*Här tror jag att en justering kommer behövas med states
         Man vill skicka första frågan så fort användraen valt kategori
         Inte vänta på att gameprocess anropas en gång till
         Som det ser ut nu så tas kategorin emot, och en lista med vald kategori
-        Men sen behöver metoden anropas igen för att hoppa ner till state QUESTION1
+        Men sen behöver metoden anropas igen för att hoppa ner till state QUESTION2
          */
-        if(state == GENRE){
+
+    //Tar alltid emot en sträng från servern (i sin tur från klienten) med vald kategori och
+    //skickar tillbaks en fråga åt gången och för spelet framåt, max tre frågor tillåtna
+    public QuestionsAndAnswers gameProcess(String theInput) {
+        QuestionsAndAnswers theOutput = null;
+        Categories chosenGenre;
+
+        //Användaren väljer en kategori och första frågan ställs
+        if (state == GENRE) {
             chosenGenre = findCategory(theInput);
             this.currentGenre = database.getListBasedOnCategory(chosenGenre);
             shuffle(currentGenre);
-            theOutput = currentGenre.get(0);
+            theOutput = currentGenre.get(0); //ställ fråga 1
+            questionsAsked++;
             state = QUESTION1;
-        } else if(state == QUESTION1 && theInput.equalsIgnoreCase("ANSWERED")){
-            theOutput = currentGenre.get(1);
-            state = QUESTION2;
-        } else if(state == QUESTION2){
-            theOutput = currentGenre.get(2);
+            //När användaren har svarat på första frågan så ställs fråga 2
+        } else if (state == QUESTION1 && theInput.equalsIgnoreCase("ANSWERED")) {
+            if (questionsAsked < questionsInRound) {
+                theOutput = currentGenre.get(1);
+                questionsAsked++;
+                state = QUESTION2;
+            }
+            //När användaren har svarat på andra frågan ställs fråga 3
+        } else if (state == QUESTION2 && theInput.equalsIgnoreCase("ANSWERED")) {
+            if (questionsAsked < questionsInRound) {
+                theOutput = currentGenre.get(2);
+                questionsAsked++;
+                state = QUESTION3;
+            }
+            //När användaren har svarat på tredje frågan återställs räknaren och spelet går tillbaks till GENRE
+        } else if (state == QUESTION3 && theInput.equalsIgnoreCase("ANSWERED")) {
+            questionsAsked = 0;
             state = GENRE;
         }
         return theOutput;
     }
 
-    public Categories findCategory(String genreString){
-        for(Categories genre : genres){
-            if (genreString.equals(genre.category)){
+    public Categories findCategory(String genreString) {
+        for (Categories genre : genres) {
+            if (genreString.equals(genre.category)) {
                 System.out.println("Kategorin: " + genre.category + " hittad");
                 return genre;
             }
         }
         return null;
     }
+
     // Metod som slumpar listan av kategorier som sedan används i RoundGUI och läggs till i varje kategori knapp.
-    public Categories randomizeCategory(){
+    public Categories randomizeCategory() {
         Random shuffle = new Random();
         int categoryIndex = shuffle.nextInt(genres.length);
         Categories shuffledCategory = genres[categoryIndex];
