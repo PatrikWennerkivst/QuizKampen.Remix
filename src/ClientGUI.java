c36c9d9261138cab33a855b7300f39a4188e37f5
Patrik Wennerkvist <patrik.wennerkvist@outlook.com>
2024-11-25 10:26:46 +0100

    kommentarer inlaggda samt uppstädning av koden

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -56,35 +56,14 @@ public class ClientGUI extends JFrame implements ActionListener {
 
     Protocol protocol = new Protocol();
 
+    //Detta är är logik som annars skulle legat i en Client klass
     ClientGUI() {
         try {
             socket = new Socket("127.0.0.1", 23478);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new ObjectInputStream(socket.getInputStream());
 
-            gameFrame.add(wholeGamePanel);
-            wholeGamePanel.add(categoryPanel, BorderLayout.CENTER);
-            categoryPanel.setBackground(Color.blue);
-            categoryPanel.add(chooseCategory);
-            chooseCategory.setHorizontalAlignment(SwingConstants.CENTER);
-
-            //Anropar metoden som slumpar en kategori från Protocol och skriver ut det med hjälp av toString
-            categoryOne.setText(protocol.randomizeCategory().category);
-            categoryTwo.setText(protocol.randomizeCategory().category);
-            categoryThree.setText(protocol.randomizeCategory().category);
-
-            categoryPanel.add(categoryOne);
-            categoryPanel.add(categoryTwo);
-            categoryPanel.add(categoryThree);
-
-            categoryOne.addActionListener(l -> choseCategory(categoryOne));
-            categoryTwo.addActionListener(l -> choseCategory(categoryTwo));
-            categoryThree.addActionListener(l -> choseCategory(categoryThree));
-
-            gameFrame.setSize(400,600);
-            gameFrame.setVisible(true);
-            gameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            gameFrame.setLocationRelativeTo(null);
+            chooseCategoryGUI();
 
             while (socket.isConnected()) {
                 // Send user message if exists
@@ -114,17 +93,45 @@ public class ClientGUI extends JFrame implements ActionListener {
         }
     }
 
-    public void choseCategory(JButton button) {
+    //Allt Gui för val av kategori
+    public void chooseCategoryGUI(){
+
+        gameFrame.add(wholeGamePanel);
+        wholeGamePanel.add(categoryPanel, BorderLayout.CENTER);
+        categoryPanel.setBackground(Color.blue);
+        categoryPanel.add(chooseCategory);
+        chooseCategory.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //Anropar metoden som slumpar en kategori från Protocol och skriver ut det med hjälp av toString
+        categoryOne.setText(protocol.randomizeCategory().category);
+        categoryTwo.setText(protocol.randomizeCategory().category);
+        categoryThree.setText(protocol.randomizeCategory().category);
+
+        categoryPanel.add(categoryOne);
+        categoryPanel.add(categoryTwo);
+        categoryPanel.add(categoryThree);
+
+        categoryOne.addActionListener(l -> sendCategoryToServer(categoryOne));
+        categoryTwo.addActionListener(l -> sendCategoryToServer(categoryTwo));
+        categoryThree.addActionListener(l -> sendCategoryToServer(categoryThree));
+
+        gameFrame.setSize(400,600);
+        gameFrame.setVisible(true);
+        gameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gameFrame.setLocationRelativeTo(null);
+    }
+        //Här skickas val av kategori till servern samt repaintar hela GUI:t
+    public void sendCategoryToServer(JButton button) {
         String selectedCategory = button.getText();
         System.out.println("CategorySelecition:" + selectedCategory);
         sendToServer(selectedCategory);
         wholeGamePanel.removeAll();
-        setClassicGame();
+        questionGUI();
         wholeGamePanel.revalidate();
         wholeGamePanel.repaint();
     }
-
-    public void setClassicGame() {
+    // Här är GUI för att visa fårgor och svar
+    public void questionGUI() {
         clickCounter = 0;
 
         for (int i = 0; i < 50; i++) {  // 5 seconds total wait
@@ -136,11 +143,13 @@ public class ClientGUI extends JFrame implements ActionListener {
                 wrongAwnser3.setText(qAndA.getThirdAnswer());
                 break;
             }
-            try {
+           /* try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
+            */
         }
         if (qAndA == null) {
             System.out.println("No questions received after waiting.");
@@ -202,13 +211,12 @@ public class ClientGUI extends JFrame implements ActionListener {
                 //Stänger ner fönstret när nextQuestionButton trycks ner
                 ((JFrame) SwingUtilities.getWindowAncestor(continueButton)).dispose();
                 startRoundGUI();
-
             }
-
-
         });
 
     }
+
+    //lägger till frågorna i respektive knapp i questionGUI
     private void updateGameQuestionAndAnswers() {
         gameQuesiton.setText(qAndA.getQuestion());
         rigthAwnser.setText(qAndA.getRightAnswer());
@@ -251,6 +259,32 @@ public class ClientGUI extends JFrame implements ActionListener {
             answersPanel.repaint();
         }
     }
+    // Metod för att hämta en ny fråga från samma kategori
+    public void getNewQuestionSameCategory() {
+        if (currentCategory == null) {
+            System.out.println("Ingen kategori vald ännu.");
+            return;
+        }
+        // Hämta alla frågor från samma kategori
+        List<QuestionsAndAnswers> sameCategoryQuestions = database.getListBasedOnCategory(currentCategory);
+
+        // Slumpa en ny fråga
+        int randomIndex = 0;
+        for (int i = 0; i < sameCategoryQuestions.size(); i++) {
+            if (i == sameCategoryQuestions.size() - 1) {
+                randomIndex = (int) (Math.random() * sameCategoryQuestions.size());
+            }
+        }
+
+        QuestionsAndAnswers newQuestion = sameCategoryQuestions.get(randomIndex);
+        // Sätt nya frågan + svar
+        setQAndA(newQuestion);
+        gameQuesiton.setText(newQuestion.getQuestion());
+        wrongAwnser1.setText(newQuestion.getFirstAnswer());
+        wrongAwnser2.setText(newQuestion.getSecondAnswer());
+        wrongAwnser3.setText(newQuestion.getThirdAnswer());
+        rigthAwnser.setText(newQuestion.getRightAnswer());
+    }
 
     public void sendToServer(String userMessage) {
         this.userMessage = userMessage;
@@ -271,15 +305,18 @@ public class ClientGUI extends JFrame implements ActionListener {
                 isQuestionReceived = false;  // Reset for next question
                 return qAndA;
             }
-            try {
+           /* try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
+            */
         }
         System.out.println("No questions received after waiting.");
         return null;
     }
+    // metod som visar poängställningen i ett GUI.
     public void startRoundGUI() {
         JFrame gameFrame = new JFrame("Round score");
         JPanel wholeGamePanel = new JPanel(new GridLayout(3, 1));
@@ -311,47 +348,17 @@ public class ClientGUI extends JFrame implements ActionListener {
         gameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    //Metod som skapar varje spelares poäng nät
     private JPanel createScorePanel() {
         JPanel scorePanel = new JPanel(new GridLayout(6, 3));
         for (int i = 0; i < 18; i++) {
             JCheckBox userCheckBox = new JCheckBox();
             userCheckBox.setEnabled(false);
-
-
             scorePanel.add(userCheckBox);
-
         }
         return scorePanel;
     }
-
-
-    // Metod för att hämta en ny fråga från samma kategori
-    public void getNewQuestionSameCategory() {
-        if (currentCategory == null) {
-            System.out.println("Ingen kategori vald ännu.");
-            return;
-        }
-        // Hämta alla frågor från samma kategori
-        List<QuestionsAndAnswers> sameCategoryQuestions = database.getListBasedOnCategory(currentCategory);
-
-        // Slumpa en ny fråga
-        int randomIndex = 0;
-        for (int i = 0; i < sameCategoryQuestions.size(); i++) {
-            if (i == sameCategoryQuestions.size() - 1) {
-                randomIndex = (int) (Math.random() * sameCategoryQuestions.size());
-            }
-        }
-
-        QuestionsAndAnswers newQuestion = sameCategoryQuestions.get(randomIndex);
-        // Sätt nya frågan + svar
-        setQAndA(newQuestion);
-        gameQuesiton.setText(newQuestion.getQuestion());
-        wrongAwnser1.setText(newQuestion.getFirstAnswer());
-        wrongAwnser2.setText(newQuestion.getSecondAnswer());
-        wrongAwnser3.setText(newQuestion.getThirdAnswer());
-        rigthAwnser.setText(newQuestion.getRightAnswer());
-    }
-
+    //Metod som skapar nätet för spelade kategorier från varje runda
     private JPanel createCategoryPanel() {
         Categories[] categories = new Categories[6];
         JPanel categoryPanel = new JPanel(new GridLayout(categories.length, 1));
@@ -360,16 +367,14 @@ public class ClientGUI extends JFrame implements ActionListener {
         }
         return categoryPanel;
     }
+
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == nextCategoryButton) {
             QuestionsAndAnswers qAa = null;
             clickCounter = 0;
-
-
-
-
+            chooseCategoryGUI();
             //Stänger ner fönstret när nextQuestionButton trycks ner
-            ((JFrame) SwingUtilities.getWindowAncestor(continueButton)).dispose();
+           // ((JFrame) SwingUtilities.getWindowAncestor(continueButton)).dispose();
         }
     }
 }
