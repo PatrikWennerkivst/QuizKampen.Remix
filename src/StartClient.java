f2a490efa70d8ac384fdd036b726be8bd802a3c3
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2024-11-28 19:32:41 +0100

    rensat upp igen

--- src/StartClient.java
+++ src/StartClient.java
@@ -1,8 +1,6 @@
 public class StartClient {
 
     public static void main(String[] args){
-            //Client client = new Client();
-            //StartGUI startGUI = new StartGUI();
         ClientGUI clientGUI = new ClientGUI();
     }
 }
